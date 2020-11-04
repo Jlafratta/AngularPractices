3f6a128a38a05226119ffc44b22463057eff1b10
Jlafratta <julian.lafratta@gmail.com>
2020-11-04 20:51:39 -0300

    routing defined for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SecondComponent } from './components/second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: '', redirectTo: '/first', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
+
+// el pathMatch indica de que manera se va matchear el path de la ruta contra la ruta en especifico
+// en el ejemplo de '' indica que si o si tiene que estar la ruta vacia
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
