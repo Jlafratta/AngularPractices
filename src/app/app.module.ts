bcbfd1153b03d17c70b9e998b2fc71bf7269c605
Jlafratta <julian.lafratta@gmail.com>
2020-11-04 18:58:33 -0300

    not-found component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
